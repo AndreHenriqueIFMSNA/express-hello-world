6d2218c23e08dec8c8f336750caca74dbdd1c45b
AndreHenriqueIFMSNA <99371362+AndreHenriqueIFMSNA@users.noreply.github.com>
2023-09-20 20:53:53 -0400

    Add meunome

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+  console.log("Just got a request!")
+  res.send('Meu nome é André Henrique Domingos da Silva!')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
